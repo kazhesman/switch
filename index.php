0b429ad11915b4da1f0b7178b4d7dd40d29d7c3e
kazhesman <kazhesman@gmail.com>
2022-09-18 15:03:18 +0300

    1

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$day = rand(1, 13);
+switch ($day) {
+    case 1:echo "Учим буквы";
+        break;
+    case 2:echo "Учим таблицу умножения";
+        break;
+    case 3:echo "Середа";
+        break;
+    case 4:echo "Учим таблицу умножения";
+        break;
+    case 5:echo "Учим таблицу умножения";
+        break; 
+    case 6:echo "Учим таблицу умножения";
+        break;
+    case 7:echo "Учим таблицу умножения";
+        break; 
+    case 8:echo "Учим таблицу умножения";
+        break;   
+    case 9:echo "Учим таблицу умножения";
+        break;
+    case 10:echo "Учим таблицу умножения";
+        break;  
+    case 11:echo "Учим таблицу умножения";
+        break;   
+    case 12:echo "Учим таблицу умножения";
+        break;                                                                                                          
+    default:echo "Помилка";
+}
